be34eb473838671a41c08243d7e7fdf0d606adb1
pavenpaven <56401108+pavenpaven@users.noreply.github.com>
2020-03-03 12:09:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import pygame
+
+class tile:
+  pass
+
+class Object:
+  pass
+
+class Turnbased_object(Object):
+  pass
+
+class playable_character(Turnbased_object):
+  pass
+
+class Enemy(Turnbased_object):
+  pass
+
+
+  
 
